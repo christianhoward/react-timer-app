27bff7e54174532d748668b47ad4321e16078226
christianhoward <christian.john.howard@gmail.com>
2017-06-24 12:35:31 -0400

    Created CountdownForm component and tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,10 +5,9 @@ const Nav = require('Nav');
 var Main = (props) => {
   return (
     <div>
-      <div>
-        <div>
-          <Nav />
-          <p>Main.jsx Rendered</p>
+      <Nav />
+      <div className="row">
+        <div className="column small-centered medium-6 large-4">
           {props.children}
         </div>
       </div>
